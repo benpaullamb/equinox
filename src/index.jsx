30140a3fe1007e90bec3044273342ca9f4c74ff3
Ben <benpaullamb@gmail.com>
2022-05-13 15:43:48 +0100

    feat: react static pre-rendering

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import App from './App';
 import ScrollToTop from './components/ScrollToTop';
 
 import './index.css';
 import 'swiper/css/bundle';
 
-import App from './App';
+const rootElement = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+const content = (
   <React.StrictMode>
     <HelmetProvider prioritizeSeoTags>
       <BrowserRouter>
@@ -22,3 +21,10 @@ root.render(
     </HelmetProvider>
   </React.StrictMode>
 );
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrateRoot(rootElement, content);
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(content);
+}
