b1a26c4145953d38ef5701965be80e118a7e499c
Ben <benpaullamb@gmail.com>
2022-05-16 16:33:38 +0100

    chore: links without text a11y

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,21 +5,30 @@ export default function SocialMedia({ className = '' }) {
     {
       icon: BsInstagram,
       url: 'https://www.instagram.com/equinoxgentlemansrefinery',
+      label: 'Instagram',
     },
     {
       icon: BsFacebook,
       url: 'https://www.facebook.com/EquinoxSouthampton',
+      label: 'Facebook',
     },
     {
       icon: BsTwitter,
       url: 'https://twitter.com/equinoxbarbers',
+      label: 'Twitter',
     },
   ];
 
   return (
     <div className={`${className} flex items-center`}>
-      {socials.map(({ icon: Icon, url }) => (
-        <a href={url} target="_blank" className="mx-2 text-3xl md:text-4xl" rel="noreferrer" key={url}>
+      {socials.map(({ icon: Icon, url, label }) => (
+        <a
+          href={url}
+          target="_blank"
+          aria-label={label}
+          className="mx-2 text-3xl md:text-4xl"
+          rel="noreferrer"
+          key={url}>
           <Icon />
         </a>
       ))}
